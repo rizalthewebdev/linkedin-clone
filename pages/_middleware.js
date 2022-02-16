29d7cff762eafb7122ac9f6e9a773163247393b0
rzlTrimelty <rzl.trimelty@gmail.com>
2022-02-16 07:13:58 +0700

    Linkedin Clone Done

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,14 +1,16 @@
-import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
-export const middleware = async (req) => {
-   if (req.nextUrl.pathname === "/") {
-      const session = await getToken({
-         req,
-         secret: process.env.JWT_SECRET,
-         secureCookie: process.env.NODE_ENV === "production",
-      });
-
-      if (!session) return NextResponse.redirect("/home");
-   }
-};
+export async function middleware(req) {
+  if (req.nextUrl.pathname === "/") {
+    const session = await getToken({
+      req,
+      secret: process.env.JWT_SECRET,
+      secureCookie: process.env.NODE_ENV === "production",
+    });
+    // You could also check for any property on the session object,
+    // like role === "admin" or name === "John Doe", etc.
+    if (!session) return NextResponse.redirect("/home");
+    // If user is authenticated, continue.
+  }
+}
